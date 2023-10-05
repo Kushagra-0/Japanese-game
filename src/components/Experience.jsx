a5ce926dfdea6c11097bda513309e78492db6b56
Kushagra-0 <kushagrashukla.dev@gmail.com>
2023-10-05 22:13:33 +0530

    Improved scenery and added japanese characters

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,14 @@
-import { OrbitControls, Cylinder } from "@react-three/drei";
+import { OrbitControls, Cylinder, MeshReflectorMaterial, Text3D } from "@react-three/drei";
 import { CylinderCollider, RigidBody } from "@react-three/rapier";
+import { Tori } from "./Tori";
+import { kanas } from "../constants";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
+
+      {/* LIGHTS */}
 
       <ambientLight intensity={1} />
       <directionalLight 
@@ -14,12 +18,58 @@ export const Experience = () => {
         color={"#9e69da"}
       />
 
+      {/* BACKGROUND */}
+
+      <mesh position={[0, -1.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[50, 50]} />
+        <MeshReflectorMaterial
+          blur={[400, 400]}
+          resolution={1024}
+          mixBlur={1}
+          mixStrength={15}
+          depthScale={1}
+          minDepthThreshold={0.85}
+          color="#dbecfb"
+          metalness={0.6}
+          roughness={1}
+        />
+      </mesh>
+
+      <Tori
+        scale={[16, 16, 16]}
+        position={[0, 0, -22]}
+        rotation-y={1.25 * Math.PI}
+      />
+      <Tori
+        scale={[10, 10, 10]}
+        position={[-8, 0, -20]}
+        rotation-y={1.4 * Math.PI}
+      />
+      <Tori 
+        scale={[10, 10, 10]}
+        position={[8, 0, -20]} 
+        rotation-y={Math.PI} 
+      />
+
+      {/* STAGE */}
+
+      <group position-y={-1}>
       <RigidBody colliders={false} type="fixed" position-y={-0.5}>
         <CylinderCollider args={[1/2, 5]} />
         <Cylinder scale={[5, 1, 5]} receiveShadow>
           <meshStandardMaterial color="white" />
         </Cylinder>
       </RigidBody>
+
+      {/* KANA */}
+
+      <Text3D
+        font={"./fonts/Noto Sans JP ExtraBold_Regular.json"}
+        size={0.82}>
+        {kanas[0].character.hiragana}
+        <meshNormalMaterial />
+      </Text3D>
+      </group>
     </>
   );
 };
